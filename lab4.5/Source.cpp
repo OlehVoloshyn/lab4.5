3ec0beb61baf0777ea88ea9389100fd87421e9ee
Олег Волошин <olegvolosin922@gmail.com>
2021-10-12 18:11:00 +0300

    lab4.5

---
+++ lab4.5/Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+
+	
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		
+		for (int i = 0; i < 10; i++)
+		{
+			x = 4.*R * rand() / RAND_MAX - 2.*R;
+			y = 4.*R * rand() / RAND_MAX - 2.*R;
+			if (((((x + R) * (x + R) + (y - R) * (y - R)) >= R * R) && ((x - R) * (x - R) + (y + R) * (y + R)) >= R * R)
+				&& (x >= -R) && (x <= R) && (y >= -R) && (y <= R))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+	return 0;
+}
+/*for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		if (((x >= -R) && (x < 0) && (y <= R - sqrt(R * R - (x + R) * (x + R))) && (y >= -R)) ||
+			((0 <= x) && (x <= R) && (y >= -R + sqrt(R * R - (x - R) * (x - R))) && y <= R))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed*/
